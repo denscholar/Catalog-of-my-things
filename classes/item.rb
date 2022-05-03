7117fafe1902beff7fb169d9e3f54232dd3b7156
david-lafontant <lafontant.david@gmail.com>
2022-05-03 11:05:52 -0600

    Add Item class properties visible in the diagram in the constructor method

--- classes/item.rb
+++ classes/item.rb
@@ -1,2 +1,15 @@
 class Item
+
+    attr_reader :id, :archived
+    attr_accessor   :label, :author, :genre, :publish_date,
+  
+    def initialize(publish_date, archived, id: rand(1..1000))
+      @publish_date = publish_date
+      @archived = archived
+      @id = id
+      @author = nil
+      @label = nil
+      @genre = nil
+    end
+
 end
