65bd8fdd1bd61de5e4ea56c2993228dafef2b80c
david-lafontant <lafontant.david@gmail.com>
2022-05-03 12:17:09 -0600

    Fix errors

--- classes/item.rb
+++ classes/item.rb
@@ -1,29 +1,28 @@
 require 'date'
 
 class Item
+  attr_reader :id, :archived
+  attr_accessor :publish_date, :author, :label, :genre
 
-    attr_reader :id, :archived
-    attr_accessor   :label, :author, :genre, :publish_date,
-  
-    def initialize(publish_date, archived, id: rand(1..1000))
-      @publish_date = publish_date
-      @archived = archived
-      @id = id
-      @author = nil
-      @label = nil
-      @genre = nil
-    end
+  def initialize(publish_date, archived, id: rand(1..1000))
+    @publish_date = publish_date
+    @archived = archived
+    @id = id
+    @author = nil
+    @label = nil
+    @genre = nil
+  end
 
-      def can_be_archived?
-        actual_year = Date.today.year
-        publishing_year = Date.parse(@publish_date).year
-        actual_year - publishing_year > 10
-      end
-    
-      def move_to_archive
-        @archived = true if can_be_archived?
-      end
-    
-      private :can_be_archived?
+  def can_be_archived?
+    actual_year = Date.today.year
+    publishing_year = Date.parse(@publish_date).year
+    actual_year - publishing_year > 10
+  end
+
+  def move_to_archive
+    @archived = true if can_be_archived?
+  end
+
+  private :can_be_archived?
 
 end
