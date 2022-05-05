2e4c19d213ef1bfe13256be8e6fc31a649f23fc2
david-lafontant <lafontant.david@gmail.com>
2022-05-05 09:25:14 -0600

    Fix linter errors

--- classes/label.rb
+++ classes/label.rb
@@ -1,18 +1,16 @@
 class Label
-    attr_accessor :title, :color
-    attr_reader :id, :items
-  
-    def initialize(id: rand(1..1000), title, color = '', items = [] )
-      @id = id
-      @title = title
-      @color = color
-      @items = items
-  
-    end
-  
-    def add_item(item)
-      @items.push(item) unless @items.include?(item)
-      item.label = self
-    end
+  attr_accessor :title, :color
+  attr_reader :id, :items
+
+  def initialize(title = ' ', color = ' ', items = [], id: rand(1..1000))
+    @title = title
+    @color = color
+    @items = items
+    @id = id
   end
-  
+
+  def add_item(item)
+    @items.push(item) unless @items.include?(item)
+    item.label = self
+  end
+end
