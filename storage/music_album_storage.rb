5a6d7797d72958d6f1d93820aa8c0b5f256b3b0c
Daniel Ufeli <danielufeli@yahoo.com>
2022-05-06 23:40:55 +0100

    Fix linter

--- storage/music_album_storage.rb
+++ storage/music_album_storage.rb
@@ -5,8 +5,8 @@ module MusicAlbumData
   def load_music_album
     if File.exist?('./data/music_album.json') && File.read('./data/music_album.json') != ''
       JSON.parse(File.read('./data/music_album.json')).each do |album|
-      MusicAlbum.new(album['published'], album['archived'],
-      album['spotify'], id: album['id'])
+        MusicAlbum.new(album['published'], album['archived'],
+                       album['spotify'], id: album['id'])
       end
     else
       []
@@ -17,7 +17,7 @@ module MusicAlbumData
     data = []
     @list_albums.each do |album|
       data.push({ genre: album.genre.name, published: album.publish_date,
-                 archived: album.archived, spotify: album.on_spotify, id: album.id })
+                  archived: album.archived, spotify: album.on_spotify, id: album.id })
     end
     File.write('./data/music_album.json', JSON.generate(data))
   end
