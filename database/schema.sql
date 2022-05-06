e3969cad783903fa8017dafa573880552c299336
david-lafontant <lafontant.david@gmail.com>
2022-05-06 09:16:42 -0600

    Update book, label, and item table

--- database/schema.sql
+++ database/schema.sql
@@ -1,28 +1,7 @@
-CREATE DATABASE catalog_of_my_thing;
-
-CREATE TABLE item (
-	id INT NOT NULL GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
-	genre_id INT,
-	author_id INT,
-	label_id INT,
-	archived BOOLEAN,
-	public_date DATE,
-	FOREIGN KEY (genre_id) REFERENCES genre (id) ON DELETE RESTRICT ON UPDATE CASCADE,
-	FOREIGN KEY (author_id) REFERENCES author (id) ON DELETE RESTRICT ON UPDATE CASCADE,
-	FOREIGN KEY (label_id) REFERENCES label (id) ON DELETE RESTRICT ON UPDATE CASCADE
-);
-
-CREATE TABLE Label (
-  id int GENERATED ALWAYS AS IDENTITY NOT NULL PRIMARY KEY,
-  title varchar(50),
-  color varchar(10)
-);
-
-CREATE TABLE Book (
-  id int GENERATED ALWAYS AS IDENTITY NOT NULL PRIMARY KEY,
-  publisher VARCHAR(50),
-  cover_state VARCHAR(10),
-  FOREIGN KEY (id) REFERENCES item (id) ON DELETE RESTRICT ON UPDATE CASCADE
+CREATE TABLE label(
+  id INT PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+  title VARCHAR(50),
+  color VARCHAR(50)
 );
 
 CREATE TABLE author (
@@ -30,6 +9,35 @@ CREATE TABLE author (
 	first_name VARCHAR(20),
 	last_name VARCHAR(20)
 );
+
+CREATE TABLE item (
+    id INT PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+    published_date DATE,
+    archived BOOLEAN,
+    genre_id INT,
+    author_id INT,
+    label_id INT,
+    FOREIGN KEY (genre_id) REFERENCES genre(id) ON DELETE SET NULL ON UPDATE CASCADE,
+    FOREIGN KEY (author_id) REFERENCES author(id) ON DELETE SET NULL ON UPDATE CASCADE,
+    FOREIGN KEY (label_id) REFERENCES label(id) ON DELETE SET NULL ON UPDATE CASCADE
+);
+
+
+CREATE TABLE book(
+  id INT PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+  published_date DATE,
+  publisher VARCHAR(100),
+  cover_state VARCHAR(4),
+  genre_id INT,
+  author_id INT,
+  label_id INT,
+  FOREIGN KEY (genre_id) REFERENCES genre(id) ON DELETE SET NULL ON UPDATE CASCADE,
+  FOREIGN KEY (author_id) REFERENCES author(id) ON DELETE SET NULL ON UPDATE CASCADE,
+  FOREIGN KEY (label_id) REFERENCES label(id) ON DELETE SET NULL ON UPDATE CASCADE
+);
+
+
+
 
 CREATE TABLE game(
 	id INT NOT NULL,
