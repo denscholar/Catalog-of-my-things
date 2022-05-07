bc74f0297062132cdae20e81ad92d26ae5e011e9
Daniel Ufeli <danielufeli@yahoo.com>
2022-05-07 04:36:42 +0100

    Fix Linters

--- test/music_album_test.rb
+++ test/music_album_test.rb
@@ -1,14 +1,14 @@
 require_relative '../classes/music_album'
 
 describe MusicAlbum do
-    context 'when creating a new Music Album' do
-      it 'return a new object who is an instance of MusicAlbum class' do
-        album = MusicAlbum.new('2010-10-10', true, id: rand(1..1000))
-        expect(album).to be_an_instance_of(MusicAlbum)
-      end
-      it 'music should be archived' do
-        album = MusicAlbum.new('2010-10-10', true, id: rand(1..1000))
-        expect(album.can_be_archived?).to be(true)
-      end
+  context 'when creating a new Music Album' do
+    it 'return a new object who is an instance of MusicAlbum class' do
+      album = MusicAlbum.new('2010-10-10', true, id: rand(1..1000))
+      expect(album).to be_an_instance_of(MusicAlbum)
     end
+    it 'music should be archived' do
+      album = MusicAlbum.new('2010-10-10', true, id: rand(1..1000))
+      expect(album.can_be_archived?).to be(true)
+    end
+  end
 end
